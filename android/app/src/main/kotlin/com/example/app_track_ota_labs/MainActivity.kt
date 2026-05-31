42b15eb4ffd26162f5598e21c36911b91596ebeb
Sebastian Otalora <sebastianotalora@tyba.com.co>
2026-05-31 00:49:41 -0500

    Updates

--- android/app/src/main/kotlin/com/example/app_track_ota_labs/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.app_track_ota_labs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/models/models.dart
+++
@@ -1,5 +0,0 @@
-library com.app_track_ota_labs.app.core.models;
-
-import 'package:flutter/material.dart';
-
-part 'app_model.dart';
